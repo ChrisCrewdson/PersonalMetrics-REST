0815a410bbee0f509467d139ff30204a23059162
ChrisCrewdson <chriscrewdson@gmail.com>
2014-03-20 13:38:55 -0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131108124450) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "records", force: true do |t|
     t.string   "data"
     t.datetime "created_at"
